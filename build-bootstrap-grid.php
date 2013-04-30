3dd36c69c63b81e7ac925c3799e55dd0f81ac0d3
Craig Freeman <craig@crowdfavorite.com>
2013-04-30 16:06:27 -0600

    Converted spaces to tabs

--- build-bootstrap-grid.php
+++ build-bootstrap-grid.php
@@ -8,173 +8,171 @@
 */
  
 class CFCT_Enable_Bootstrap {
-        public $block_classes_change_map = array();
- 
-        public $old_block_classname_to_new = array(
-            // All New Grid classes in Carrington
-            'c4-1234' => 'span12',
+	public $block_classes_change_map = array();
 
-            'c6-1234' => 'span8',
-            'c6-3456' => 'span8',
+	public $old_block_classname_to_new = array(
+		// All New Grid classes in Carrington
+		'c4-1234' => 'span12',
 
-            'c4-12' => 'span6',
-            'c4-34' => 'span6',
+		'c6-1234' => 'span8',
+		'c6-3456' => 'span8',
 
-            'c6-12' => 'span4',
-            'c6-34' => 'span4',
-            'c6-56' => 'span4'
-        );
-        
-        public function __construct() {
-                foreach ($this->old_block_classname_to_new as $new => $old) {
-                        $this->push_block_class_change($old, $new);
-                }
+		'c4-12' => 'span6',
+		'c4-34' => 'span6',
 
-                return $this;
-        }
+		'c6-12' => 'span4',
+		'c6-34' => 'span4',
+		'c6-56' => 'span4'
+	);
+	
+	public function __construct() {
+		foreach ($this->old_block_classname_to_new as $new => $old) {
+			$this->push_block_class_change($old, $new);
+		}
 
-        public function attach_hooks() {
-                // Restore generated block ID class
-                add_filter(
-                        'cfct-generated-row-classes',
-                        array($this, 'add_generated_in_row_classes'),
-                        10, 3
-                );
+		return $this;
+	}
 
-                add_filter(
-                        'cfct-row-html',
-                        array($this, 'restore_row_html'),
-                        10, 3
-                );
+	public function attach_hooks() {
+		// Restore generated block ID class
+		add_filter(
+			'cfct-generated-row-classes',
+			array($this, 'add_generated_in_row_classes'),
+			10, 3
+		);
 
-                add_filter(
-                    'cfct-row-defaults',
-                    array($this, 'enable_proper_row_classes'),
-                    10
-                );
-                
-                add_filter(
-                        'cfct-block-template',
-                        array($this, 'restore_block_template'),
-                        10, 2
-                );
-                            
-                $block_class_filters = array(
-                        /* Full */
-                        'cfct-block-c4-1234-classes',
-                        
-                        /* Halves */
-                        'cfct-block-c4-12-classes',
-                        'cfct-block-c4-34-classes',
-                        
-                        /* Thirds */
-                        'cfct-block-c6-12-classes',
-                        'cfct-block-c6-34-classes',
-                        'cfct-block-c6-56-classes',
-                        
-                        /* 2 Thirds */
-                        'cfct-block-c6-1234-classes',
-                        'cfct-block-c6-3456-classes'
-                );
-                
-                foreach($block_class_filters as $filter_key) {
-                        add_filter(
-                                $filter_key,
-                                array($this, 'restore_block_classes'),
-                                10, 2
-                        );
-                }
-        }
-        
-        /**
-         * Backwards-compat row markup
-         */
-        public function restore_row_html($html, $classname, $classes) {
+		add_filter(
+			'cfct-row-html',
+			array($this, 'restore_row_html'),
+			10, 3
+		);
 
-                return '<div id="{id}" class="{class}">{blocks}</div>';
-        }
-        
-        /**
-         * Brings back block ID
-         */
-        public function restore_block_template($html, $block_instance) {
-                return '<div id="{id}" class="{class}">{modules}</div>';
-        }
-                
-        public function push_block_class_change($old, $new) {
-                $this->block_classes_change_map[] = array(
-                        'old' => cfct_tpl::extract_classes($old),
-                        'new' => cfct_tpl::extract_classes($new)
-                );
-        }
-        
-        public function add_generated_in_row_classes($classes, $module_types, $row_instance) {
-                $generated_classes = $row_instance->add_in_row_classes($module_types); 
+		add_filter(
+			'cfct-row-defaults',
+			array($this, 'enable_proper_row_classes'),
+			10
+		);
+		
+		add_filter(
+			'cfct-block-template',
+			array($this, 'restore_block_template'),
+			10, 2
+		);
+						
+		$block_class_filters = array(
+			/* Full */
+			'cfct-block-c4-1234-classes',
+			
+			/* Halves */
+			'cfct-block-c4-12-classes',
+			'cfct-block-c4-34-classes',
+			
+			/* Thirds */
+			'cfct-block-c6-12-classes',
+			'cfct-block-c6-34-classes',
+			'cfct-block-c6-56-classes',
+	
+			/* 2 Thirds */
+			'cfct-block-c6-1234-classes',
+			'cfct-block-c6-3456-classes'
+		);
+			
+		foreach($block_class_filters as $filter_key) {
+			add_filter(
+				$filter_key,
+				array($this, 'restore_block_classes'),
+				10, 2
+			);
+		}
+	}
+	
+	/**
+	 * Backwards-compat row markup
+	 */
+	public function restore_row_html($html, $classname, $classes) {
+		return '<div id="{id}" class="{class}">{blocks}</div>';
+	}
+	
+	/**
+	 * Brings back block ID
+	 */
+	public function restore_block_template($html, $block_instance) {
+		return '<div id="{id}" class="{class}">{modules}</div>';
+	}
+			
+	public function push_block_class_change($old, $new) {
+		$this->block_classes_change_map[] = array(
+			'old' => cfct_tpl::extract_classes($old),
+			'new' => cfct_tpl::extract_classes($new)
+		);
+	}
+	
+	public function add_generated_in_row_classes($classes, $module_types, $row_instance) {
+		$generated_classes = $row_instance->add_in_row_classes($module_types); 
 
-                return array_merge($classes, $generated_classes);
-        }
-        
-        public function restore_classes($ch_ch_ch_changes, $classes) {
-                foreach ($ch_ch_ch_changes as $pair) {
-                        /* (turn and face the strain) */
-                        $intersect = array_intersect($classes, $pair['new']);
-                        /* Does the row have the same classes that we've recorded as new?
-                        Then it's a match, so add the equivalent old classes. */
-                        if (count($intersect) == count($pair['new'])) {
+		return array_merge($classes, $generated_classes);
+	}
+	
+	public function restore_classes($ch_ch_ch_changes, $classes) {
+		foreach ($ch_ch_ch_changes as $pair) {
+			/* (turn and face the strain) */
+			$intersect = array_intersect($classes, $pair['new']);
+			/* Does the row have the same classes that we've recorded as new?
+			Then it's a match, so add the equivalent old classes. */
+			if (count($intersect) == count($pair['new'])) {
+				$classes = array_merge($classes, $pair['old']);
+			}
+		}
+		
+		return cfct_tpl::clean_classes($classes);
+	}
+	
+	public function restore_block_classes($classes, $block_instance) {
+		$classes = $this->restore_classes(
+				$this->block_classes_change_map, $classes
+		);
+		return $classes;
+	}
+	
+	public function enable_proper_row_classes($defaults) {
+		$defaults['row_class'] = 'row-fluid';
 
-                                $classes = array_merge($classes, $pair['old']);
-                        }
-                }
-                
-                return cfct_tpl::clean_classes($classes);
-        }
-        
-        public function restore_block_classes($classes, $block_instance) {
-                $classes = $this->restore_classes(
-                        $this->block_classes_change_map, $classes
-                );
-                return $classes;
-        }
-        
-        public function enable_proper_row_classes($defaults) {
-            $defaults['row_class'] = 'row-fluid';
+		return $defaults;
+	}
 
-            return $defaults;
-        }
+	/**
+	 * A bit janky, but since we don't have error handling in WP,
+	 * do a feature check to make sure this version of Build is compatible with
+	 * this plugin.
+	 */
+	public static function check_features() {
+		if (!function_exists('cfct_build')) {
+			return new WP_Error('function not found', 'Carrington Build needs to be activated for \"Twitter Bootstrap grid for Carrigton Buildadd \" to take effect.');
+		}
+		if (!class_exists('cfct_tpl')) {
+			return new WP_Error('class not found', 'Class cfct_tpl does not exist. You probably need to install a newer version of Carrington Build.');
+		}
+		if (!function_exists('cfct_build_register_row')) {
+			return new WP_Error('function not found', 'Function cfct_build_register_row does not exist.');
+		}
+	}
 
-        /**
-         * A bit janky, but since we don't have error handling in WP,
-         * do a feature check to make sure this version of Build is compatible with
-         * this plugin.
-         */
-        public static function check_features() {
-                if (!function_exists('cfct_build')) {
-                        return new WP_Error('function not found', 'Carrington Build needs to be activated for \"Twitter Bootstrap grid for Carrigton Build\" to take effect.');
-                }
-                if (!class_exists('cfct_tpl')) {
-                        return new WP_Error('class not found', 'Class cfct_tpl does not exist. You probably need to install a newer version of Carrington Build.');
-                }
-                if (!function_exists('cfct_build_register_row')) {
-                        return new WP_Error('function not found', 'Function cfct_build_register_row does not exist.');
-                }
-        }
-        
-        /**
-         * Hook this into init()
-         */
-        public static function init() {
-                $diagnostics = self::check_features();
-                if (is_wp_error($diagnostics)) {
-                        $cb = create_function('', 'echo "<div class=\'message error\'>
-        <p>'.$diagnostics->get_error_message().'</p>
+	/**
+	 * Hook this into init()
+	 */
+	public static function init() {
+		$diagnostics = self::check_features();
+		if (is_wp_error($diagnostics)) {
+				$cb = create_function('', 'echo "<div class=\'message error\'>
+	<p>'.$diagnostics->get_error_message().'</p>
 </div>";');
-                        add_action('admin_notices', $cb);
-                }
-                else {
-                        $instance = new CFCT_Enable_Bootstrap();
-                        $instance->attach_hooks();
-                }
-        }
+			add_action('admin_notices', $cb);
+		}
+		else {
+			$instance = new CFCT_Enable_Bootstrap();
+			$instance->attach_hooks();
+		}
+	}
 }
 add_action('init', array('CFCT_Enable_Bootstrap', 'init'));
 ?>
